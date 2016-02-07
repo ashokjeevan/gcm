f789f4e54cc6cbd64d2d460c25e466c74040b51f
AJ <ashokjeevan@gmail.com>
2016-02-07 14:40:48 +0530

    Modified the notification icon and read me file

--- app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MyGcmListenerService.java
+++ app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MyGcmListenerService.java
@@ -82,7 +82,7 @@ public class MyGcmListenerService extends GcmListenerService {
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Trial App")
                 .setContentText(message)
                 .setAutoCancel(true)
